9bea9fee6136f6b3eb6478ed0f6bbd54222c7f9b
Akhil Suryawanshi <akhilsuryawanshi6356@gmail.com>
2023-10-23 23:14:51 +0530

    Time: 1 ms (76.31%), Space: 39.5 MB (58.82%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,19 @@
+ class Solution {
+    public boolean isPowerOfFour(int n) {
+         
+         
+        if (n <= 0) {
+            return false;
+        }
+
+          
+        if ((n & (n - 1)) == 0) {
+           
+            
+            return (n - 1) % 3 == 0;
+        }
+
+         
+        return false;
+    }
+}
