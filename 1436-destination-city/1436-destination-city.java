decd3699177a6ec71cb7f8e81c6bfe7427fea773
Akhil Suryawanshi <akhilsuryawanshi6356@gmail.com>
2024-01-27 17:54:00 +0530

    Time: 2 ms (77.89%), Space: 43.3 MB (34.28%) - LeetHub

--- 1436-destination-city/1436-destination-city.java
+++ 1436-destination-city/1436-destination-city.java
@@ -2,9 +2,9 @@ class Solution {
     public String destCity(List<List<String>> paths) {
         Map<String,String> map=new HashMap<String,String>();
         for(int i=0;i<paths.size();i++)
-        {   List l=paths.get(i);   
-            String key=(String)l.get(0);
-                String value=(String)l.get(1);
+        {   List<String> l=paths.get(i);   
+            String key=l.get(0);
+                String value=l.get(1);
             map.put(key,value);
         }
         String find=paths.get(0).get(0);
